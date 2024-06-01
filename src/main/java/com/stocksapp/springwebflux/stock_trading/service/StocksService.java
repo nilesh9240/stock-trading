9790486e29ae1420522ad8746bafda5013f9c051
nilesh <nilesh9240@gmail.com>
2024-06-01 16:38:21 +0530

    added peek operators

--- src/main/java/com/stocksapp/springwebflux/stock_trading/service/StocksService.java
+++ src/main/java/com/stocksapp/springwebflux/stock_trading/service/StocksService.java
@@ -7,6 +7,8 @@ import com.stocksapp.springwebflux.stock_trading.exception.StockNotFoundExceptio
 import com.stocksapp.springwebflux.stock_trading.model.Stock;
 import com.stocksapp.springwebflux.stock_trading.repository.StocksRepository;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
@@ -19,19 +21,34 @@ import java.util.List;
 @AllArgsConstructor
 public class StocksService {
 
+    private static final Logger log = LoggerFactory.getLogger(StocksService.class);
+
     @Autowired
     private StocksRepository stocksRepository;
 
     public Mono<StockResponse> getOneStock(String id){
         return stocksRepository.findById(id)
                 .map(StockResponse::fromModel)
-                .switchIfEmpty(Mono.error(new StockNotFoundException("Stock not found with id: " + id)));
+                .switchIfEmpty(Mono.error(
+                        new StockNotFoundException(
+                                "Stock not found with id: " + id)))
+                                .doFirst(() -> log.info("Retrieving Stock with id: {}", id))
+                                .doOnNext(stock -> log.info("Stock found: {}", stock))
+                                .doOnError(ex -> log.error("Something went wrong happened while retrieving stock with id: {}", id, ex))
+                                .doOnTerminate(() -> log.info("Finalized retrieving stock"))
+                                .doFinally(signalType -> log.info("Finished stock retrieval with signal type: {}", signalType));
     }
 
     public Flux<StockResponse> getAllStocks(BigDecimal priceGreaterThan) {
         return stocksRepository.findAll()
                 .filter(stock -> stock.getPrice().compareTo(priceGreaterThan) > 0)
-                .map(StockResponse::fromModel);
+                .map(StockResponse::fromModel)
+                .doFirst(() -> log.info("Retrieving all stocks"))
+                .doOnNext(stock -> log.info("Stock found: {}", stock))
+                .doOnError(ex -> log.error("Something went wrong happened while retrieving stocks", ex))
+                .doOnTerminate(() -> log.info("Finalized retrieving stock"))
+                .doFinally(signalType -> log.info("Finished stock retrieval with signal type: {}", signalType));
+
 
     }
 
